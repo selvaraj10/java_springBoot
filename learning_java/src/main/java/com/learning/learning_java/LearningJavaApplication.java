f31d4953533a35e3623fa58b7b6a0d9d59c436b2
natasel <selvanataraj45@gmail.com>
2024-01-12 13:55:24 +0530

    Initial commit

--- learning_java/src/main/java/com/learning/learning_java/LearningJavaApplication.java
+++ learning_java/src/main/java/com/learning/learning_java/LearningJavaApplication.java
@@ -3,11 +3,10 @@ package com.learning.learning_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class LearningJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningJavaApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
