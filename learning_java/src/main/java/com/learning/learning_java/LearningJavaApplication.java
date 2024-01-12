4b619fcf1b08f250c737d0241ab1d1742f985f86
selvaraj10 <156407358+selvaraj10@users.noreply.github.com>
2024-01-12 12:34:10 +0530

    Initial commit

---
+++ learning_java/src/main/java/com/learning/learning_java/LearningJavaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.learning_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJavaApplication.class, args);
+	}
+
+}
