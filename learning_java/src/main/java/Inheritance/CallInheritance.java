63152a3b79674bba9f229d4daff75019409784bc
natasel <selvanataraj45@gmail.com>
2024-02-16 20:57:03 +0530

    Initial commit

--- learning_java/src/main/java/Inheritance/CallInheritance.java
+++ learning_java/src/main/java/Inheritance/CallInheritance.java
@@ -1,8 +1,29 @@
 package Inheritance;
 
+import java.util.Scanner;
+
 public class CallInheritance {
-//    public static void main(String[] args) {
-        KolliuThatha kolliuThatha = new KolliuThatha();
-//    }
+////    public static void main(String[] args) {
+//        KolliuThatha kolliuThatha = new KolliuThatha();
+////    }
+public static void main(String args[]){
+
+        for(int i=0;i<args.length;i++)
+                System.out.println(args[i]);
 
 }
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d = scan.nextDouble();
+//        String s="";
+//        while(scan.hasNext())
+//        {
+//                s=scan.nextLine();
+//        }
+//        scan.close();
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
+}
+
+
